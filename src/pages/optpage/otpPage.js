7862076a693d15cc547e8831e0239e9ad1f2fd09
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2024-03-16 02:18:52 -0400

    update the professional page and new data and updating chatpage,add otp page as well

---
+++ src/pages/optpage/otpPage.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/navbarLogResPage/nav';
+import OTP from '../../components/otp/otp';
+import Footer from '../../components/footer/footer';
+
+function ResetPage() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className='log'>
+      <OTP/>
+      </div>
+     
+     <Footer/>
+    </div>
+  );
+}
+
+export default ResetPage;
